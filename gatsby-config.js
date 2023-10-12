8269564db0b1c00dde2a62105e1d8650cf587eec
chifavz <chifavz@gmail.com>
2023-10-11 20:23:49 -0400

    corrected netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     "gatsby-plugin-image",
-    ,"gatsby-plugin-netlify"
+    "gatsby-plugin-netlify",
     {
       resolve: "gatsby-source-filesystem",
       options: {
